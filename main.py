2d28d99e94e54faa989cff640e9ab312401713b1
mantra <mantra@mantras-MacBook-Pro.local>
2021-04-12 01:07:50 +0530

    update comments

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    # function to test discord bot is loogged in or not
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+    '''
+    this function used to received request from bot the send response back to discord bot
+    params:
+        message: message object received from discoed bot contains request string and author and bot related information
+    '''
+
     print(message,"\n")
     # print(dir(message))
     if message.author == client.user:
@@ -19,15 +26,18 @@ async def on_message(message):
     if message.content.startswith('$hi'):
         await message.channel.send('hey')
     
+    #when a msg recevied with !recent string below block of codes resonse back to bot
+    # for a given string and user if their any recent histroy present then it will reply top 6 link based on hostorical search data
     if message.content.startswith('!recent'):
         q=message.content.split("!recent")[1]
         rest=get_data(q,message.author.id)
         print(rest)
         await message.channel.send(rest)
-        
         if len(rest)==0:
             await message.channel.send("No result found")
 
+    #when a msg recevied with !google string below block of codes resonse back to bot
+    # below function search for the request string with google search api and return top 6 link back to bot
     if message.content.startswith('!google'):
         q=message.content.split("!google")[1]
         url='https://www.googleapis.com/customsearch/v1?key=AIzaSyCnUxZ_ZSDGmrEOcV4tTf0yDrG-OxzbPpA&cx=013384490937324803065:etsy46pgcig&q='
--- service.py
+++ service.py
@@ -2,12 +2,23 @@ from elasticsearch import Elasticsearch,exceptions
 es = Elasticsearch()
 
 def insert(doc):
+    '''
+    this function is used to store serach data of a particular user
+    params:
+        doc: array of dictionary contains data generated from google search of a particular user.
+    '''
     for i in doc:
         res = es.index(index="user_history", body=i)
         print(res['result'])
 
 
 def get_data(query,id):
+    '''
+    this function is used to get the hostorical serach data of a particular user
+    params:
+        query: string to be serached for
+        id: user id
+    '''
     q={
         "query" : {
             "bool" : {
@@ -28,8 +39,8 @@ def get_data(query,id):
         }
     }
     try:
-        res = es.search(index="user_history",body=q)
-        res=res['hits']['hits'][:6]
+        res = es.search(index="user_history",body=q) # searching in elastic seach
+        res=res['hits']['hits'][:6] # only fetching first 6 record
         if len(res)>0:
             res=[i['_source']['link'] for i in res]
             res='\n'.join(res)
